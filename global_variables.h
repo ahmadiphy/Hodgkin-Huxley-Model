aa7f42b8d9b0989034e246f3d041a5bf7413231f
Abolfazl Ahmadi rahmat <38430894+ahmadiphy@users.noreply.github.com>
2019-01-11 01:00:30 +0330

    Update global_variables.h

--- global_variables.h
+++ global_variables.h
@@ -1,6 +1,5 @@
 #ifndef GLOBAL_VARIABLES_H
 #define GLOBAL_VARIABLES_H
-//extern double h;
 extern double G_Na,G_K,G_L,C_m,v_K,v_Na,v_L,dt,v_0;
 extern double a,b,c,d,h;
 #endif // GLOBAL_VARIABLES_H
